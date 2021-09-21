1cbc301d382b5b1344f0e3e40ecdeeead7a3839d
Rajat <rajat.karnal@gmail.com>
2021-09-22 03:55:35 +0530

    'switch-case added'

--- _9_Conditionals.java
+++ _9_Conditionals.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class _9_Conditionals {
     public static void main(String[] args) {
 
@@ -9,5 +11,31 @@ public class _9_Conditionals {
             System.out.println("You are underage!");
         }
 
+        // Switch-case
+        Scanner S = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = S.next();
+        S.close();
+        // Normal switch
+        switch (name) {
+            case "Rajat":
+                System.out.println("You are a good boy");
+                System.out.println("Have a good life!");
+                break;
+            case "Mohit":
+                System.out.println("You are a bad boy");
+            default:
+                System.out.println("You are a smart boy");
+                break;
+        }
+        // Enhanced switch
+        switch (name) {
+            case "Rajat" -> {
+                System.out.println("You are a good boy");
+                System.out.println("Have a good life!");
+            }
+            case "Mohit" -> System.out.println("You are a bad boy");
+            default -> System.out.println("You are a smart boy");
+        }
     }
 }
